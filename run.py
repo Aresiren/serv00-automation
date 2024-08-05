dd5280a896f64d759fe16f9b90ce3adfeb37cd8c
wangyan <baiyouming2011@126.com>
2024-08-06 01:12:08 +0800

    Revert "添加密钥ssh连接"
    This reverts commit 373a8fec1b65a3dcac744d963d58512029650f13.

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ import asyncio
 from pyppeteer import launch
 import random
 import time
-import io
 
 '''
 
@@ -17,7 +16,7 @@ SSH_INFO:
 [
   {"hostname": "xx.serv00.com","username": "serv00的账号", "password": "serv00的密码", "panel": "panel6.serv00.com"},
   {"hostname": "s5.serv00.com","username": "ct8的账号", "password": "ct8的密码", "panel": "panel.ct8.pl"},
-  {"hostname": "s6.serv00.com","username": "user2", "password": "password2", "panel": "panel6.serv00.com", "pkeyflag":"0"}
+  {"hostname": "s6.serv00.com","username": "user2", "password": "password2", "panel": "panel6.serv00.com"}
 ]
 
 LOGIN_TYPE:
@@ -39,7 +38,6 @@ MAIL:
 # 从环境变量中获取
 mail = os.getenv('MAIL')
 push = os.getenv('PUSH')
-pkey = os.getenv('PKEY')
 LOGIN_TYPE = os.getenv('LOGIN_TYPE') # ssh or telegram or http
 ssh_info_str = os.getenv('SSH_INFO', '[]')
 host_infos = json.loads(ssh_info_str)
@@ -90,25 +88,11 @@ def ssh_multiple_connections(host_infos, command) -> str:
         hostname = host_info['hostname']
         username = host_info['username']
         password = host_info['password']
-        pkeyflag ="0"
-        pkeyflagstr = host_info['pkeyflag']
-        if 'pkeyflag' in host_info.keys():
-            pkeyflag=pkeyflagstr
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            
-            if '0' == pkeyflag:
-                # 密码连接ssh
-                ssh.connect(hostname=hostname, port=22, username=username, password=password)
-            else:
-                # 密钥连接ssh
-                mykey = paramiko.RSAKey.from_private_key(io.StringIO(pkey))
-                ssh.connect(hostname=hostname, port=22, username=username, pkey=mykey, timeout=30)
-            
+            ssh.connect(hostname=hostname, port=22, username=username, password=password)
             stdin, stdout, stderr = ssh.exec_command(command)
-            stdin.close()
-
             stdout_content = stdout.read().decode().strip()
             print('ssh 回显信息：',stdout_content)
             stdout_contents.append(stdout_content)
